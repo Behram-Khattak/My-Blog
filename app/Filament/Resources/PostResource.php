85836e050e06299e24dbd29b49c147f7ef674fbc
Behram-Khattak <behramkttk9@gmail.com>
2023-12-20 23:04:50 +0500

    Created posts page with form to post blog

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -7,11 +7,13 @@ use App\Filament\Resources\PostResource\RelationManagers;
 use App\Models\Post;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Forms\Set;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
 
 class PostResource extends Resource
 {
@@ -23,8 +25,44 @@ class PostResource extends Resource
     {
         return $form
             ->schema([
-                //
-            ]);
+                Forms\Components\Select::make('category')
+                                            ->options([
+                                                'Web Development',
+                                                'Artificial Intelligence',
+                                            ])
+                                            ->placeholder('Select Category')
+                                            ->required(),
+
+                Forms\Components\TextInput::make('title')
+                                        ->live(onBlur: true)
+                                        ->afterStateUpdated(function (Set $set, $state) {
+                                            $set('slug', Str::slug($state));
+                                        })
+                                        ->required(),
+
+                Forms\Components\RichEditor::make('content')
+                                        ->columnSpanFull()
+                                        ->required(),
+
+                Forms\Components\TextInput::make('slug')
+                                            ->disabled()
+                                            ->required(),
+
+                Forms\Components\TagsInput::make('tags')
+                                            ->suggestions([
+                                                'tailwindcss',
+                                                'alpinejs',
+                                                'laravel',
+                                                'livewire',
+                                            ])
+                                            ->reorderable()
+                                            ->required(),
+
+                Forms\Components\FileUpload::make('thumbnail')
+                                            ->columnSpanFull()
+                                            ->required()
+            ])
+            ->columns(2);
     }
 
     public static function table(Table $table): Table
