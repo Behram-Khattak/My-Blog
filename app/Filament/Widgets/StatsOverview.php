5486b76085545d1cdf7bcdc4b1dd97aa74e1a6a2
Behram-Khattak <behramkttk9@gmail.com>
2024-01-02 20:55:55 +0500

    Added widgets to Dashboard page

---
+++ app/Filament/Widgets/StatsOverview.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Category;
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\Tag;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class StatsOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Categories', Category::count())
+                ->icon('heroicon-o-list-bullet'),
+            Stat::make('Posts', Post::count())
+                ->icon('heroicon-o-clipboard-document-list'),
+            Stat::make('Tags', Tag::count())
+                ->icon('heroicon-o-tag'),
+            Stat::make('Comments', Comment::count())
+                ->icon('heroicon-o-chat-bubble-left-right'),
+        ];
+    }
+}
