f3483f361525c9f595c27af6349e18ef5f18d382
Behram-Khattak <behramkttk9@gmail.com>
2024-02-18 15:46:43 +0500

    Created a custom theme.css file in resources/view/filament/css folder also added the filement folder path to vite.config.js file to to reload assets on file save

--- resources/views/livewire/chat-comment.blade.php
+++ resources/views/livewire/chat-comment.blade.php
@@ -1,4 +1,8 @@
-<div class="text-center">
+<div class="flex gap-4">
+    <p class="text-red-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, beatae numquam dolorem modi porro dolores dignissimos. Assumenda, magnam recusandae placeat fugiat asperiores esse eaque officiis impedit cum. Laboriosam, voluptas tenetur.
+    </p>
+
     <p class="text-red-500">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, beatae numquam dolorem modi porro dolores dignissimos. Assumenda, magnam recusandae placeat fugiat asperiores esse eaque officiis impedit cum. Laboriosam, voluptas tenetur.
     </p>
